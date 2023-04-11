1c52315d2513034f69824201d48549dccede131c
Norbert Salavecz <norbert.salavecz@stud.uni-corvinus.hu>
2023-04-11 08:57:08 +0200

    Játék absztrakció kezelése

---
+++ week06/week06/Abstractions/Toy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace week06.Abstractions //átmásoltuk a labdát, de ezt írjuk át erre a mappára!!!
+{
+    public class Ball : Label //itt rename-el a Ballt átírtuk Toy-ra, de csak azután szabad, hogy átírtuk a mappát
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint; //itt is megváltoztattuk a nevet, rename-el
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics); //ez kell, hogy használja a rajzolást
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height); //ez rajzol rá egy kék kört
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
