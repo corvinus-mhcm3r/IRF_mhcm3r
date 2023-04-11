afc1ab97acf2689bd1b7d98aefde01a5ad34aa7a
Norbert Salavecz <norbert.salavecz@stud.uni-corvinus.hu>
2023-04-11 08:30:40 +0200

    Labda létrehozása

---
+++ week06/week06/Entities/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace week06.Entities
+{
+    class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Ball_Paint; //ez meghívja a 
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics); //ez kell, hogy használja a rajzolást
+        }
+
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height); //ez rajzol rá egy kék kört
+        }
+        
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
