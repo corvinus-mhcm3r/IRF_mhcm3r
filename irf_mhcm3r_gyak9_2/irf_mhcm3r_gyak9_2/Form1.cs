1a5c81f6570982f63406476ec4be18ba69fc23ba
Norbert Salavecz <norbert.salavecz@stud.uni-corvinus.hu>
2023-05-09 09:02:50 +0200

    Adatok betöltése 2

--- irf_mhcm3r_gyak9_2/irf_mhcm3r_gyak9_2/Form1.cs
+++ irf_mhcm3r_gyak9_2/irf_mhcm3r_gyak9_2/Form1.cs
@@ -22,6 +22,8 @@ namespace irf_mhcm3r_gyak9_2
         {
             InitializeComponent();
             Population.AddRange(GetPopulation("nép-teszt.csv"));
+            BirthProbabilities.AddRange(GetBirthProbabilities("születés.csv"));
+            DeathProbabilities.AddRange(GetDeathProbabilities("halál.csv"));
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -30,7 +32,6 @@ namespace irf_mhcm3r_gyak9_2
 
             using (var sr = new StreamReader(csvpath, Encoding.Default))
             {
-                sr.ReadLine();
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine().Split(';');
@@ -45,5 +46,48 @@ namespace irf_mhcm3r_gyak9_2
 
             return population;
         }
+
+        public List<BirthProbability> GetBirthProbabilities(string csvpath)
+        {
+            var birthprobabilities = new List<BirthProbability>();
+
+            using (var sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    birthprobabilities.Add(new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = byte.Parse(line[1]),
+                        P = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return birthprobabilities;
+        }
+
+        public List<DeathProbability> GetDeathProbabilities(string csvpath)
+        {
+            var deathprobabilities = new List<DeathProbability>();
+
+            using (var sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    deathprobabilities.Add(new DeathProbability()
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = int.Parse(line[1]),
+                        P = double.Parse(line[2])
+                    });
+                }
+            }
+
+            return deathprobabilities;
+        }
+
     }
 }
